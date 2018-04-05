a324a4784b9ec1cd47e5e968026393dde3898930
aliciawyy <rainingilove@gmail.com>
2018-04-05 19:31:56 +0200

    refactor daily vol method

--- learning/label.py
+++ learning/label.py
@@ -1,14 +1,13 @@
-import pandas as pd
-
-
 class Price(object):
     def __init__(self, price_series):
         self.data = price_series
 
-    def ewm_vol(self, span=100):
-        price = self.data
-        df = price.index.searchsorted(price.index - pd.Timedelta(days=1))
-        df = df[df > 0]
-        df = pd.Series(price.index[df - 1], index=price.index[price.shape[0] - df.shape[0]:])
-        df1 = price.loc[df.index] / price.loc[df.values].values - 1
-        return df1.ewm(span=span).std()
+    @property
+    def daily_ret(self):
+        return self.data.asfreq("B", method="pad").pct_change()
+
+    def daily_ewm_vol(self, span=100):
+        return self.daily_ret.ewm(span=span).std()
+
+    def daily_rolling_vol(self, window=100):
+        return self.daily_ret.rolling(window).std()
--- tests/test_label.py
+++ tests/test_label.py
@@ -11,7 +11,12 @@ class PriceTest(FMLTestCase):
         cls.data = DataHandler().get_time_series_data("SPY")
         cls.close = cls.data["Close"]
 
-    def test_daily_vol(self):
+    def test_daily_ewm_vol(self):
         price = label.Price(self.close["2016-06":"2016-12"])
-        df = price.ewm_vol(20)
+        df = price.daily_ewm_vol(50)
         self.assert_frame_equal(df, "close_ewm_vol")
+
+    def test_daily_rolling_vol(self):
+        price = label.Price(self.close["2016-06":"2016-12"])
+        df = price.daily_rolling_vol(50)
+        self.assert_frame_equal(df, "close_rolling_vol")
